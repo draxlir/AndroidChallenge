8c0ba3752e2a73d66de68830fb0ba32ffc150104
lbj3018a <julien-labadens@orange.fr>
2022-03-18 10:28:38 +0100

    init menu start

--- app/src/main/java/com/example/androidchallenge/MainActivity.java
+++ app/src/main/java/com/example/androidchallenge/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -23,7 +24,10 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide(); // hide top bar
         setContentView(R.layout.activity_main);
 
-        // Test pour la gameview, à enlever
+
+    }
+
+    public void clickStart(View v){
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
