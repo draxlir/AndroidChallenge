1ec7022fc9afd1bb6a204729648071a2287c05e5
Maxence Demougeot <maxence.demougeot1@gmail.com>
2022-03-18 11:25:44 +0100

    Création des debris et de la planète Mars

---
+++ app/src/main/java/com/example/androidchallenge/Constants.java
@@ -0,0 +1,15 @@
+package com.example.androidchallenge;
+
+public final class Constants {
+
+    private Constants(){
+
+    }
+
+    public final static int MIN_DEBRIS_RADIUS = 50;
+    public final static int MAX_DEBRIS_RADIUS = 100;
+    public final static int PLAYER_RADIUS = 30;
+    public final static int MARS_RADIUS = 150;
+
+    public final static int DEBRIS_NUMBER = 20;
+}
--- app/src/main/java/com/example/androidchallenge/GameView.java
+++ app/src/main/java/com/example/androidchallenge/GameView.java
@@ -9,8 +9,14 @@ import android.view.SurfaceView;
 
 import androidx.annotation.NonNull;
 
+import com.example.androidchallenge.domain.Debris;
+import com.example.androidchallenge.domain.Mars;
 import com.example.androidchallenge.threads.GameDrawThread;
 import com.example.androidchallenge.threads.GameUpdateThread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 
@@ -20,10 +26,30 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
     private final int SCREEN_WIDTH = this.getResources().getDisplayMetrics().widthPixels;
     private final int SCREEN_HEIGHT = this.getResources().getDisplayMetrics().heightPixels;
 
+    private final List<Debris> debris = new ArrayList<>();
+    private Mars mars;
+
     public GameView(Context context) {
         super(context);
         getHolder().addCallback(this);
         setFocusable(true);
+    }
+
+    public void marsCreation() {
+        Random rnd = new Random();
+        int posX = rnd.nextInt((SCREEN_WIDTH * 5 -Constants.MARS_RADIUS) - Constants.MARS_RADIUS) + Constants.MARS_RADIUS;
+        int posY = rnd.nextInt((SCREEN_HEIGHT * 5 -Constants.MARS_RADIUS) - Constants.MARS_RADIUS) + Constants.MARS_RADIUS;
+        mars = new Mars((float) posX, (float) posY, Constants.MARS_RADIUS);
+    }
+
+    public void debrisCreation() {
+        Random rnd = new Random();
+        for(int i=0; i<Constants.DEBRIS_NUMBER; i++){
+            int radius = rnd.nextInt(100-50) + 50;
+            int posX = rnd.nextInt((SCREEN_WIDTH * 5 - radius) - radius) + radius;
+            int posY = rnd.nextInt((SCREEN_HEIGHT * 5 - radius) - radius) + radius;
+            debris.add(new Debris((float) posX, (float) posY, radius));
+        }
     }
 
     public void draw(Canvas canvas) {
@@ -42,6 +68,9 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+        debrisCreation();
+        marsCreation();
+
         threadDraw.setRunning(true);
         threadDraw.start();
         threadUpdate.setRunning(true);
